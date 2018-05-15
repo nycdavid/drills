237be81bc6ede49b77a2e0aea87bfbc6b891d645
David Ko <david.ko@velvetreactor.com>
2018-05-15 07:52:54 -0400

    Attempt at implementing Manacher's

--- leetcode/palinsubstr/palinsubstr.go
+++ leetcode/palinsubstr/palinsubstr.go
@@ -1,47 +1,25 @@
 package palinsubtr
 
 import (
-	"strings"
+	"fmt"
 )
 
 func longestPalindrome(s string) string {
-	var j int
-	var ans, substr string
-	for j < len(s) {
-		substr = s[0 : j+1]
-		if isPalindrome(substr) {
-			ans = MaxLen(ans, substr)
-		}
-		j++
-	}
-	for i := range s {
-		substr = s[i:len(s)]
-		if isPalindrome(substr) {
-			ans = MaxLen(ans, substr)
-		}
-	}
-	return ans
-}
-
-func isPalindrome(s string) bool {
-	lgth := len(s)
-	if lgth == 1 {
-		return true
-	}
-	var rev []string
+	caps := make([]int, len(s))
 	for idx := range s {
-		revIdx := lgth - 1 - idx
-		rev = append(rev, string(s[revIdx]))
+		i := idx
+		j := idx
+		ln := 1
+		for i > 0 && j < len(s) {
+			i--
+			j++
+			if s[i] == s[j] {
+				ln += 2
+			}
+			caps[idx] = ln
+		}
+		caps[idx] = ln
 	}
-	if strings.Join(rev, "") == s {
-		return true
-	}
-	return false
-}
-
-func MaxLen(s1, s2 string) string {
-	if len(s1) > len(s2) {
-		return s1
-	}
-	return s2
+	fmt.Println(caps)
+	return ""
 }
