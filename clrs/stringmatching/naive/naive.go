323bca02a4a8197e3f05cee5a65023f3ef505c39
David Ko <david.ko@velvetreactor.com>
2018-05-17 06:58:30 -0400

    Add Printer interface for mocking

--- clrs/stringmatching/naive/naive.go
+++ clrs/stringmatching/naive/naive.go
@@ -1,7 +1,17 @@
 package naive
 
+import (
+	"fmt"
+)
+
 type Printer interface {
 	Println(a ...interface{}) (int, error)
+}
+
+type StdoutPrinter struct{}
+
+func (s StdoutPrinter) Println(a ...interface{}) (int, error) {
+	return fmt.Println(a...)
 }
 
 type NaiveMatcher struct {
@@ -9,6 +19,21 @@ type NaiveMatcher struct {
 	p   Printer
 }
 
-func (nm *NaiveMatcher) Find(substr string) (bool, int) {
+func NewNaiveMatcher(str string, p Printer) *NaiveMatcher {
+	return &NaiveMatcher{str: str, p: StdoutPrinter{}}
+}
+
+func (nm *NaiveMatcher) Find(pattern string) (bool, int) {
+	t := nm.lenT()
+	p := len(pattern)
+	for s := 0; s < t-p; s++ {
+		if pattern[0:p] == nm.str[s:s+p] {
+			nm.p.Println(fmt.Sprintf("Pattern occurs with shift %d", s))
+		}
+	}
 	return false, 0
+}
+
+func (nm *NaiveMatcher) lenT() int {
+	return len(nm.str)
 }
--- clrs/stringmatching/naive/naive_test.go
+++ clrs/stringmatching/naive/naive_test.go
@@ -1,17 +1,38 @@
 package naive
 
 import (
+	"bytes"
+	"fmt"
 	"testing"
 )
 
+type BufferPrinter struct {
+	buf *bytes.Buffer
+}
+
+func (bp BufferPrinter) Println(a ...interface{}) (int, error) {
+	for _, s := range a {
+		bp.buf.WriteString(s.(string))
+	}
+	return 0, nil
+}
+
 func TestNaiveStringMatcher(t *testing.T) {
-	m := &NaiveMatcher{str: "abcdefg"}
-	exists, shift := m.Find("bc")
+	var buf bytes.Buffer
+	bp := BufferPrinter{buf: &buf}
+	mat := &NaiveMatcher{str: "abcdefg", p: bp}
+	exists, shift := mat.Find("bc")
 
 	if exists != true {
 		t.Error("Expected to find substring")
 	}
 	if shift != 1 {
 		t.Error("Expected shift to be 1")
+	}
+	expected := "Pattern occurs with shift 1"
+	got := buf.String()
+	if got != expected {
+		msg := fmt.Sprintf("Expected string to be %s, got %s", expected, got)
+		t.Error(msg)
 	}
 }
