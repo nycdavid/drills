40a08414f1965a7c9a4bab37f7b449d61059c82e
David Ko <david.ko@velvetreactor.com>
2018-05-18 06:43:59 -0400

    Add test for example 32.1-1

--- clrs/stringmatching/naive/naive_test.go
+++ clrs/stringmatching/naive/naive_test.go
@@ -15,7 +15,7 @@ func (bp BufferPrinter) Println(a ...interface{}) (int, error) {
 	for _, s := range a {
 		bp.buf.WriteString(s.(string))
 	}
-	return 0, nil
+	return fmt.Println(a)
 }
 
 func TestNaiveStringMatcher(t *testing.T) {
@@ -29,6 +29,26 @@ func TestNaiveStringMatcher(t *testing.T) {
 	}
 	expected := "Pattern occurs with shift 1\nPattern occurs with shift 7"
 	got := buf.String()
+	if strings.TrimSpace(got) != strings.TrimSpace(expected) {
+		msg := fmt.Sprintf("Expected string to be %s, got %s", expected, got)
+		t.Error(msg)
+	}
+}
+
+func Test32_1_1(t *testing.T) {
+	T := "000010001010001"
+	p := "0001"
+	var buf bytes.Buffer
+	bp := BufferPrinter{buf: &buf}
+	mat := &NaiveMatcher{str: T, p: bp}
+
+	exists := mat.Find(p)
+	expected := "Pattern occurs with shift 1\nPattern occurs with shift 5\nPattern occurs with shift 11"
+	got := buf.String()
+
+	if exists != true {
+		t.Error("Expected to find substring")
+	}
 	if strings.TrimSpace(got) != strings.TrimSpace(expected) {
 		msg := fmt.Sprintf("Expected string to be %s, got %s", expected, got)
 		t.Error(msg)
