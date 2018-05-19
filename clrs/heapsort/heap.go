802bbbc6a056913f5a598848d72ea7f30648d425
David Ko <david.ko@velvetreactor.com>
2018-05-19 07:04:47 -0400

    Add heap and heap node methods

--- clrs/heapsort/heap.go
+++ clrs/heapsort/heap.go
@@ -4,6 +4,35 @@ type Heap struct {
 	arr []int
 }
 
+type HeapNode struct {
+	arr []int
+	Pos int
+}
+
 func (h *Heap) Length() int {
 	return len(h.arr)
+}
+
+func (h *Heap) Node(pos int) *HeapNode {
+	return &HeapNode{Pos: pos, arr: h.arr}
+}
+
+func (n *HeapNode) Left() *HeapNode {
+	newPos := 2 * n.Pos
+	return &HeapNode{Pos: newPos, arr: n.arr}
+}
+
+func (n *HeapNode) Right() *HeapNode {
+	newPos := 2*n.Pos + 1
+	return &HeapNode{Pos: newPos, arr: n.arr}
+}
+
+func (n *HeapNode) Parent() *HeapNode {
+	newPos := n.Pos / 2
+	return &HeapNode{Pos: newPos, arr: n.arr}
+}
+
+func (n *HeapNode) Val() int {
+	idx := n.Pos - 1
+	return n.arr[idx]
 }
--- clrs/heapsort/heap_test.go
+++ clrs/heapsort/heap_test.go
@@ -30,3 +30,58 @@ func TestHeapLength(t *testing.T) {
 		t.Error(msg)
 	}
 }
+
+func TestHeapNode(t *testing.T) {
+	heapArr := []int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}
+	heap := &Heap{arr: heapArr}
+
+	expected := 16
+	got := heap.Node(1).Val()
+
+	if expected != got {
+		msg := fmt.Sprintf("Expected heapNode to be %d, got %d", expected, got)
+		t.Error(msg)
+	}
+}
+
+func TestHeapNodeLeft(t *testing.T) {
+	heapArr := []int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}
+	heap := &Heap{arr: heapArr}
+	node := heap.Node(1)
+
+	expected := heapArr[1]
+	got := node.Left().Val()
+
+	if expected != got {
+		msg := fmt.Sprintf("Expected left to be %d, got %d", expected, got)
+		t.Error(msg)
+	}
+}
+
+func TestHeapNodeRight(t *testing.T) {
+	heapArr := []int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}
+	heap := &Heap{arr: heapArr}
+	node := heap.Node(1)
+
+	expected := heapArr[2]
+	got := node.Right().Val()
+
+	if expected != got {
+		msg := fmt.Sprintf("Expected left to be %d, got %d", expected, got)
+		t.Error(msg)
+	}
+}
+
+func TestHeapNodeParent(t *testing.T) {
+	heapArr := []int{16, 14, 10, 8, 7, 9, 3, 2, 4, 1}
+	heap := &Heap{arr: heapArr}
+	node := heap.Node(7)
+
+	expected := heapArr[2]
+	got := node.Parent().Val()
+
+	if expected != got {
+		msg := fmt.Sprintf("Expected left to be %d, got %d", expected, got)
+		t.Error(msg)
+	}
+}
